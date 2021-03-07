c62a4120beaebedc91a2dd289412f403400b95c8
Hans <2655098959@qq.com>
2021-03-07 13:33:31 +0800

    Add Splay template

--- SplayTree.h
+++ SplayTree.h
@@ -1,16 +1,25 @@
 #ifndef SPLAYTREE_H
 #define SPLAYTREE_H
 
+#define SPLAY_DEBUG
+#define SPLAY_PORTABLE
+
+template <typename T>
+class SplayTree;
+
+template<typename T>
 class SplayTreeNode {
 public:
-    using PtrSplayNode = SplayTreeNode*;
+    using PtrSplayNode = SplayTreeNode<T>*;
     SplayTreeNode();
-    SplayTreeNode(const int& key);
+    SplayTreeNode(const T& key);
     ~SplayTreeNode();
 
-    int getKey();
+    T getKey();
 
+#ifndef SPLAY_PORTABLE
     static int getSize(PtrSplayNode);
+#endif
 
     static void Zig(PtrSplayNode);
     static void Zag(PtrSplayNode);
@@ -20,37 +29,60 @@ public:
     static void ZagZig(PtrSplayNode);
     static void ZagZag(PtrSplayNode);
 
-    friend class SplayTree;
+    friend class SplayTree<T>;
 
 private:
-    int key;
+    T key;
     PtrSplayNode left;
     PtrSplayNode right;
     PtrSplayNode parent;
+#ifndef SPLAY_PORTABLE
     int count;
     int size;
+#endif
 };
 
-SplayTreeNode::SplayTreeNode() {}
-SplayTreeNode::SplayTreeNode(const int& key) : key(key), left(nullptr), right(nullptr), parent(nullptr), size(1), count(1) {}
-SplayTreeNode::~SplayTreeNode() {}
+template <typename T>
+SplayTreeNode<T>::SplayTreeNode() {}
 
-int SplayTreeNode::getKey() {
+template <typename T>
+SplayTreeNode<T>::SplayTreeNode(const T& key) :
+    key(key),
+    left(nullptr),
+    right(nullptr),
+    parent(nullptr)
+#ifndef SPLAY_PORTABLE
+    ,size(1),
+    count(1)
+#endif
+    {}
+
+template <typename T>
+SplayTreeNode<T>::~SplayTreeNode() {}
+
+template <typename T>
+T SplayTreeNode<T>::getKey() {
     return key;
 }
 
-int SplayTreeNode::getSize(PtrSplayNode node) {
+#ifndef SPLAY_PORTABLE
+template <typename T>
+int SplayTreeNode<T>::getSize(PtrSplayNode node) {
     return node == nullptr ? 0 : node->size;
 }
+#endif
 
-void SplayTreeNode::Zig(PtrSplayNode K1) {
+template <typename T>
+void SplayTreeNode<T>::Zig(PtrSplayNode K1) {
     PtrSplayNode K2 = K1->parent;
     K2->left = K1->right;
     K1->right = K2;
     K1->parent = K2->parent;
     K2->parent = K1;
+#ifndef SPLAY_PORTABLE
     K2->size = getSize(K2->left) + getSize(K2->right) + K2->count;
     K1->size = getSize(K1->left) + getSize(K1->right) + K1->count;
+#endif
     if (K2->left != nullptr) {
         K2->left->parent = K2;
     }
@@ -63,14 +95,17 @@ void SplayTreeNode::Zig(PtrSplayNode K1) {
     }
 }
 
-void SplayTreeNode::Zag(PtrSplayNode K1) {
+template <typename T>
+void SplayTreeNode<T>::Zag(PtrSplayNode K1) {
     PtrSplayNode K2 = K1->parent;
     K2->right = K1->left;
     K1->left = K2;
     K1->parent = K2->parent;
     K2->parent = K1;
+#ifndef SPLAY_PORTABLE
     K2->size = getSize(K2->left) + getSize(K2->right) + K2->count;
     K1->size = getSize(K1->left) + getSize(K1->right) + K1->count;
+#endif
     if (K2->right != nullptr) {
         K2->right->parent = K2;
     }
@@ -83,58 +118,92 @@ void SplayTreeNode::Zag(PtrSplayNode K1) {
     }
 }
 
-void SplayTreeNode::ZigZig(PtrSplayNode node) {
+template <typename T>
+void SplayTreeNode<T>::ZigZig(PtrSplayNode node) {
     Zig(node->parent);
     Zig(node);
 }
 
-void SplayTreeNode::ZigZag(PtrSplayNode node) {
+template <typename T>
+void SplayTreeNode<T>::ZigZag(PtrSplayNode node) {
     Zag(node);
     Zig(node);
 }
 
-void SplayTreeNode::ZagZig(PtrSplayNode node) {
+template <typename T>
+void SplayTreeNode<T>::ZagZig(PtrSplayNode node) {
     Zig(node);
     Zag(node);
 }
 
-void SplayTreeNode::ZagZag(PtrSplayNode node) {
+template <typename T>
+void SplayTreeNode<T>::ZagZag(PtrSplayNode node) {
     Zag(node->parent);
     Zag(node);
 }
 
+template <typename T>
 class SplayTree {
 public:
-    using PtrSplayNode = SplayTreeNode*;
+    using PtrSplayNode = SplayTreeNode<T>*;
     SplayTree();
     ~SplayTree();
 
-    void insert(const int& x);
-    void remove(const int& x);
-    PtrSplayNode find(const int& x);
-    int kthElement(int k);
-    PtrSplayNode lowerBound(const int& x);
-    PtrSplayNode upperBound(const int& x);
+    void insert(const T& x);
+    void remove(const T& x);
+    PtrSplayNode find(const T& x);
     PtrSplayNode predecessor(PtrSplayNode node);
     PtrSplayNode successor(PtrSplayNode node);
-    int getRank(const int& x);
+
+#ifndef SPLAY_PORTABLE
+    T kthElement(int k);
+    PtrSplayNode lowerBound(const T& x);
+    PtrSplayNode upperBound(const T& x);
+    int getRank(const T& x);
+#endif
+
+#ifdef SPLAY_DEBUG
     void printTree();
+#endif
 
 private:
     PtrSplayNode root;
 
     void splay(PtrSplayNode node);
+    void deleteSubtree(PtrSplayNode node);
+
+#ifndef SPLAY_PORTABLE
     PtrSplayNode realFindKth(int k);
+#endif
+
+#ifdef SPLAY_DEBUG
     void printTree(PtrSplayNode node, int depth);
+#endif
 
 };
 
-SplayTree::SplayTree() : root(nullptr) {}
-SplayTree::~SplayTree() {}
+template <typename T>
+SplayTree<T>::SplayTree() : root(nullptr) {}
 
-void SplayTree::insert(const int& x) {
+template <typename T>
+SplayTree<T>::~SplayTree() {
+    deleteSubtree(root);
+}
+
+template <typename T>
+void SplayTree<T>::deleteSubtree(PtrSplayNode node) {
+    if (node == nullptr) {
+        return;
+    }
+    deleteSubtree(node->left);
+    deleteSubtree(node->right);
+    delete node;
+}
+
+template <typename T>
+void SplayTree<T>::insert(const T& x) {
     if (root == nullptr) {
-        root = new SplayTreeNode(x);
+        root = new SplayTreeNode<T>(x);
         return;
     }
     PtrSplayNode cur = root, new_node;
@@ -143,25 +212,31 @@ void SplayTree::insert(const int& x) {
             if (cur->right != nullptr) {
                 cur = cur->right;
             } else {
-                cur->right = new SplayTreeNode(x);
+                cur->right = new SplayTreeNode<T>(x);
                 new_node = cur->right;
                 new_node->parent = cur;
+#ifndef SPLAY_PORTABLE
                 cur->size++;
+#endif
                 break;
             }
         } else if (cur->key > x) {
             if (cur->left != nullptr) {
                 cur = cur->left;
             } else {
-                cur->left = new SplayTreeNode(x);
+                cur->left = new SplayTreeNode<T>(x);
                 new_node = cur->left;
                 new_node->parent = cur;
+#ifndef SPLAY_PORTABLE
                 cur->size++;
+#endif
                 break;
             }
         } else {
+#ifndef SPLAY_PORTABLE
             cur->size++;
             cur->count++;
+#endif
             new_node = cur;
             break;
         }
@@ -169,8 +244,43 @@ void SplayTree::insert(const int& x) {
     splay(new_node);
 }
 
+
+template <typename T>
+void SplayTree<T>::remove(const T& x) {
+    PtrSplayNode position = find(x);
+    if (position == nullptr) { // Not found
+        return;
+    }
+    splay(position);
+
+#ifndef SPLAY_PORTABLE
+    if (position->count > 1)  {
+        position->count--;
+        position->size--;
+        return;
+    }
+#endif
+
+    PtrSplayNode pre = predecessor(position);
+    if (pre == nullptr) { // There is no left subtree
+        root = position->right;
+    } else {
+        position->left->parent = nullptr;
+        splay(pre);
+        pre->right = position->right;
+#ifndef SPLAY_PORTABLE
+        pre->size = SplayTreeNode<T>::getSize(pre->left) + SplayTreeNode<T>::getSize(pre->right) + pre->count;
+#endif
+    }
+    if (position->right != nullptr) {
+        position->right->parent = pre;
+    }
+    delete(position);
+}
+
 // Splay the node to top
-void SplayTree::splay(PtrSplayNode node) {
+template <typename T>
+void SplayTree<T>::splay(PtrSplayNode node) {
     while (node->parent != nullptr) {
         if (node->parent->parent != nullptr) {
             PtrSplayNode parent_node = node->parent;
@@ -178,23 +288,23 @@ void SplayTree::splay(PtrSplayNode node) {
             int config = (parent_node->left == node) * 2 + (gparent_node->left == parent_node);
             switch (config) {
                 case 0:
-                    SplayTreeNode::ZagZag(node);
+                    SplayTreeNode<T>::ZagZag(node);
                     break;
                 case 1:
-                    SplayTreeNode::ZigZag(node);
+                    SplayTreeNode<T>::ZigZag(node);
                     break;
                 case 2:
-                    SplayTreeNode::ZagZig(node);
+                    SplayTreeNode<T>::ZagZig(node);
                     break;
                 case 3:
-                    SplayTreeNode::ZigZig(node);
+                    SplayTreeNode<T>::ZigZig(node);
                     break;
             }
         } else {
             if (node->parent->left == node) {
-                SplayTreeNode::Zig(node);
+                SplayTreeNode<T>::Zig(node);
             } else {
-                SplayTreeNode::Zag(node);
+                SplayTreeNode<T>::Zag(node);
             }
         }
     }
@@ -203,7 +313,8 @@ void SplayTree::splay(PtrSplayNode node) {
 
 // Yield the pointer to the node which contains key x
 // return nullptr if not found 
-SplayTree::PtrSplayNode SplayTree::find(const int& x) {
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::find(const T& x) {
     PtrSplayNode cur = root;
     while (cur != nullptr) {
         if (cur->key > x) {
@@ -220,65 +331,12 @@ SplayTree::PtrSplayNode SplayTree::find(const int& x) {
     return cur;
 }
 
-int SplayTree::getRank(const int& x) {
-    int rank = 0;
-    PtrSplayNode cur = root;
-    // Check for Memory leak!
-    while (cur != nullptr) {
-        if (cur->key > x) {
-            cur = cur->left;
-        } else if (cur->key < x) {
-            rank += SplayTreeNode::getSize(cur->left) + cur->count;
-            cur = cur->right;
-        } else {
-            break;
-        }
-    }
-    if (cur != nullptr) {
-        rank += SplayTreeNode::getSize(cur->left) + 1;
-        splay(cur);
-        return rank;
-    } else {
-        return -1;
-    }
-}
-
-SplayTree::PtrSplayNode SplayTree::lowerBound(const int& x) {
-    PtrSplayNode cur = root, ans = nullptr;
-    while (cur != nullptr) {
-        if (cur->key < x) {
-            cur = cur->right;
-        } else {
-            ans = cur;
-            cur = cur->left;
-        }
-    }
-    if (ans != nullptr) {
-        splay(ans);
-    }
-    return ans;
-}
-
-SplayTree::PtrSplayNode SplayTree::upperBound(const int& x) {
-    PtrSplayNode cur = root, ans = nullptr;
-    while (cur != nullptr) {
-        if (cur->key <= x) {
-            cur = cur->right;
-        } else {
-            ans = cur;
-            cur = cur->left;
-        }
-    }
-    if (ans != nullptr) {
-        splay(ans);
-    }
-    return ans;
-}
 
 // Find the precurser of the node
 // return nullptr if it doesn't exist
 // We assume predecessor(null) = max;
-SplayTree::PtrSplayNode SplayTree::predecessor(PtrSplayNode node) {
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::predecessor(PtrSplayNode node) {
     if (node == nullptr) {
         PtrSplayNode ans = root;
         if (ans != nullptr) {
@@ -306,7 +364,8 @@ SplayTree::PtrSplayNode SplayTree::predecessor(PtrSplayNode node) {
 // Find the postcurser of the node
 // return nullptr if it doesn't exist
 // We assume successor(null) = min
-SplayTree::PtrSplayNode SplayTree::successor(PtrSplayNode node) {
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::successor(PtrSplayNode node) {
     if (node == nullptr) {
         PtrSplayNode ans = root;
         if (ans != nullptr) {
@@ -331,35 +390,69 @@ SplayTree::PtrSplayNode SplayTree::successor(PtrSplayNode node) {
     return ans;
 }
 
-void SplayTree::remove(const int& x) {
-    PtrSplayNode position = find(x);
-    if (position == nullptr) { // Not found
-        return;
-    }
-    splay(position);
-    if (position->count > 1) {
-        position->count--;
-        position->size--;
-        return;
-    }
 
-    PtrSplayNode pre = predecessor(position);
-    if (pre == nullptr) { // There is no left subtree
-        root = position->right;
+#ifndef SPLAY_PORTABLE
+template <typename T>
+int SplayTree<T>::getRank(const T& x) {
+    int rank = 0;
+    PtrSplayNode cur = root;
+    // Check for Memory leak!
+    while (cur != nullptr) {
+        if (cur->key > x) {
+            cur = cur->left;
+        } else if (cur->key < x) {
+            rank += SplayTreeNode<T>::getSize(cur->left) + cur->count;
+            cur = cur->right;
+        } else {
+            break;
+        }
+    }
+    if (cur != nullptr) {
+        rank += SplayTreeNode<T>::getSize(cur->left) + 1;
+        splay(cur);
+        return rank;
     } else {
-        position->left->parent = nullptr;
-        splay(pre);
-        pre->right = position->right;
-        pre->size = SplayTreeNode::getSize(pre->left) + SplayTreeNode::getSize(pre->right) + pre->count;
+        return -1;
     }
-    if (position->right != nullptr) {
-        position->right->parent = pre;
+}
+
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::lowerBound(const T& x) {
+    PtrSplayNode cur = root, ans = nullptr;
+    while (cur != nullptr) {
+        if (cur->key < x) {
+            cur = cur->right;
+        } else {
+            ans = cur;
+            cur = cur->left;
+        }
     }
-    delete(position);
+    if (ans != nullptr) {
+        splay(ans);
+    }
+    return ans;
+}
+
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::upperBound(const T& x) {
+    PtrSplayNode cur = root, ans = nullptr;
+    while (cur != nullptr) {
+        if (cur->key <= x) {
+            cur = cur->right;
+        } else {
+            ans = cur;
+            cur = cur->left;
+        }
+    }
+    if (ans != nullptr) {
+        splay(ans);
+    }
+    return ans;
 }
 
 // Wrapper for realFindKth
-int SplayTree::kthElement(int k) {
+template <typename T>
+T SplayTree<T>::kthElement(int k) {
     auto node = realFindKth(k);
     if (node == nullptr) {
         return -1;
@@ -370,13 +463,14 @@ int SplayTree::kthElement(int k) {
 
 // Find the Kth element
 // return null if K is illegal
-SplayTree::PtrSplayNode SplayTree::realFindKth(int k) {
+template <typename T>
+typename SplayTree<T>::PtrSplayNode SplayTree<T>::realFindKth(int k) {
     PtrSplayNode cur = root;
     while (cur != nullptr) {
-        if (SplayTreeNode::getSize(cur->left) >= k) {
+        if (SplayTreeNode<T>::getSize(cur->left) >= k) {
             cur = cur->left;
-        } else if (SplayTreeNode::getSize(cur->left) + cur->count < k) {
-            k -= SplayTreeNode::getSize(cur->left) + cur->count;
+        } else if (SplayTreeNode<T>::getSize(cur->left) + cur->count < k) {
+            k -= SplayTreeNode<T>::getSize(cur->left) + cur->count;
             cur = cur->right;
         } else {
             break;
@@ -387,13 +481,17 @@ SplayTree::PtrSplayNode SplayTree::realFindKth(int k) {
     }
     return cur;
 }
+#endif
 
-void SplayTree::printTree() {
+#ifdef SPLAY_DEBUG
+template <typename T>
+void SplayTree<T>::printTree() {
     std::cout << "============================" << std::endl;
     printTree(root, 2);
 }
 
-void SplayTree::printTree(PtrSplayNode node, int depth) {
+template <typename T>
+void SplayTree<T>::printTree(PtrSplayNode node, int depth) {
     if (node == nullptr) {
         return;
     }
@@ -401,9 +499,14 @@ void SplayTree::printTree(PtrSplayNode node, int depth) {
     for (int i = 1; i <= depth; i++) {
         std::cout << '-';
     }
-    std::cout << ' ' << node->key << " : " << node->count << std::endl;
+    std::cout << ' ' << node->key
+#ifndef SPLAY_PORTABLE
+        << " : " << node->count
+#endif
+        << std::endl;
     printTree(node->right, depth + 2);
 }
+#endif
 
 
 #endif // SPLAYTREE_H
